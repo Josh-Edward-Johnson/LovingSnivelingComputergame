a733ea8f95783d59207b9d64b102ec179874abe9
Josh-Edward-Johnson <88618366+Josh-Edward-Johnson@users.noreply.github.com>
2021-11-16 19:41:11 +0300

    main.cs
    this is a code from LovingSnivelingComputergame repl

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //This program will divide, subtract, multiply and add to numbers and more!
+    int a = 10;
+    int b = 5;
+    int c = 11;
+    int d = 4;
+    int e = 100;
+    int f = 50;
+    int g = 800;
+    int h = 900;
+    Console.WriteLine((a / b).ToString());
+    Console.WriteLine("10 divided by 5 is equal to 2 ^^^");
+    Console.WriteLine((c * d).ToString());
+    Console.WriteLine("11 multiplied by 4 equal to 44 ^^^");
+    Console.WriteLine((e - f).ToString());
+    Console.WriteLine("100 subtracted by 50 equal to 50 ^^^");
+    Console.WriteLine((g + h).ToString());
+    Console.WriteLine("adding 900 to 800 is equal to 1700 ^^^");
+  }
+}
